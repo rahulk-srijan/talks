1e992d0af798e09fbee05a73b32dd6f84455bd6b
unknown <Ishan Mahajan@XFT000076048002.ads.mckinsey.com>
2014-03-18 13:54:37 +0530

    Correcting the prop names in download tracking JS

--- sites/all/modules/custom/mckc_omniture/omniture_js/custom_variables.js
+++ sites/all/modules/custom/mckc_omniture/omniture_js/custom_variables.js
@@ -3,13 +3,11 @@
 	$('.download-video').delegate('a', 'click', function(){
         var rawpageTitle = $(document).attr('title').split('|');
 		var pageTitle = $.trim(rawpageTitle[0]);
-        s.linkTrackVars = 'prop1,prop5,prop6';
+        s.linkTrackVars = 'prop1,prop5';
         s.linkTrackEvents = 'None';
         s.prop5 = pageTitle;
         var pidx = Drupal.settings.myOmniture.pidX;
         s.prop1 = pidx;
-        s.prop6 = test;
-        alert(pidx);
         s.tl(this, 'd');
 	});
 })(jQuery);
