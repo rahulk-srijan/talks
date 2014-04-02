e43a0191e74f914af5dc644d3702bfe7bf9fe59d
nitesh kumar-xft <nitesh_kumar-xft@external.mckinsey.com>
2014-04-02 13:28:55 +0530

    removing duration from upload field tpl

--- sites/all/themes/vedio_library/templates/views-view-field--field-upload-video.tpl.php
+++ sites/all/themes/vedio_library/templates/views-view-field--field-upload-video.tpl.php
@@ -27,7 +27,7 @@ $video_uri = $row->field_field_upload_video[0]['raw']['uri'];
 $video_file_name = $row->field_field_upload_video[0]['raw']['playablefiles'][0]->filename;
 $root_path = $_SERVER['DOCUMENT_ROOT'];
 $video_path = $root_path . '/sites/default/files/videos/original/' . $video_file_name;
-$video_duration = video_library_get_video_duration($video_uri);
+//$video_duration = video_library_get_video_duration($video_uri);
 //$get_nid= $row->nid;
 //
 //$video_duration = video_library_get_video_duration($get_nid);
@@ -43,5 +43,6 @@ $video_duration = video_library_get_video_duration($video_uri);
     print "<a href=" . $base_url . "/" . $node_path_alias . "><img src=" . $base_url . "/sites/all/themes/vedio_library/css/images/play_icon.png /></a>";
     ?>
 </div>
+<!--
 <div class="video_duration">
-    <?php print $video_duration; ?></div>
+    <?php //print $video_duration; ?></div> --!>
