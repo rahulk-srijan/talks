4496188b0d3a9c881b8ef50e8c4bb8e9dc5c9a6e
unknown <Ishan Mahajan@XFT000076048002.ads.mckinsey.com>
2014-04-08 12:43:58 +0530

    Adding channel omniture variable when downloading or searching a video.

--- sites/all/modules/custom/mckc_omniture/omniture_js/custom_variables.js
+++ sites/all/modules/custom/mckc_omniture/omniture_js/custom_variables.js
@@ -3,11 +3,13 @@
 	$('.download-video').delegate('a', 'click', function(){
         var rawpageTitle = $(document).attr('title').split('|');
 		var pageTitle = $.trim(rawpageTitle[0]);
-        s.linkTrackVars = 'prop1,prop5';
+        s.linkTrackVars = 'prop1,prop3,prop5';
         s.linkTrackEvents = 'None';
         s.prop5 = pageTitle;
         var pidx = Drupal.settings.myOmniture.pidX;
+        var channel = Drupal.settings.myOmniture.channel;
         s.prop1 = pidx;
+        s.prop3 = channel;
         s.tl(this, 'd');
 	});
 })(jQuery);
