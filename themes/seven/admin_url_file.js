04f27e90fc3badee96775b0dc2a53a42a35a904c
prshntdrupal <Prashant Goel@XFT000101057002.ads.mckinsey.com>
2014-06-06 15:39:38 +0530

    js fix for dual url

--- themes/seven/admin_url_file.js
+++ themes/seven/admin_url_file.js
@@ -18,3 +18,20 @@ jQuery('document').ready(function() {
         }
     });
 });
+
+jQuery('document').ready(function() {
+    jQuery('#organisation-video-library-gallery form').each(function() { 
+        var href = jQuery(this).attr('action');
+        if(href){
+            var ex_href = href.split("/");
+            if(ex_href[1]){
+                //var t = 'globalcomm';
+                var str_length = parseInt(ex_href[1].length+1);
+                if(ex_href[1]== ex_href[2]){
+                    var sub_href= href.substr(str_length,9000);
+                    jQuery(this).attr('action', sub_href);
+                }
+            }
+        }
+    });
+});
