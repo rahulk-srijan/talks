8277bb62f687fab6b7eee57da7db8040b87c127a
prashant <prashant@srijan.in>
2014-07-07 13:24:04 +0530

    remove external websites

--- sites/all/themes/vedio_library/templates/html.tpl.php
+++ sites/all/themes/vedio_library/templates/html.tpl.php
@@ -111,8 +111,7 @@ if (userNav.indexOf('Trident/6.0') >= 0 && userNav.indexOf('MSIE 7.0')>= 0) { /*
 
 </script>
 <!--[if lt IE 9]>
-    <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
-    <script src="scripts/css3-mediaqueries.js"></script>
+    <script src="/scripts/css3-mediaqueries.js"></script>
 <![endif]-->
 <!-- <script src="/sites/all/libraries/jwplayer/jwplayer.js"></script> -->
  <!--<script>jwplayer.key="Guox+nuMLWKTuNNNaAnBSWq9ep4UEQMc3m+a1A=="</script>-->
